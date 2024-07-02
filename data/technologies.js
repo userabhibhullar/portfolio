6503a5255b625b8c6aec0ea31a21e645b1afdbeb
userabhibhullar <iabhibhullaryt@gmail.com>
2024-07-02 18:03:40 +0530

    Fixed image path for Github Pages

--- data/technologies.js
+++ data/technologies.js
@@ -3,37 +3,37 @@ export const languages = [
     id: "js",
     name: "Javascript",
     level: "Intermediate",
-    image: "/images/logos/logo-javascript.svg",
+    image: "images/logos/logo-javascript.svg",
   },
   {
     id: "python",
     name: "Python",
     level: "Intermediate",
-    image: "/images/logos/logo-python.svg",
+    image: "images/logos/logo-python.svg",
   },
   {
     id: "cpp",
     name: "C++",
     level: "Intermediate",
-    image: "/images/logos/logo-cpp.svg",
+    image: "images/logos/logo-cpp.svg",
   },
   {
     id: "dart",
     name: "Dart",
     level: "Beginner",
-    image: "/images/logos/logo-dart.svg",
+    image: "images/logos/logo-dart.svg",
   },
   {
     id: "csharp",
     name: "C#",
     level: "Beginner",
-    image: "/images/logos/logo-csharp.svg",
+    image: "images/logos/logo-csharp.svg",
   },
   {
     id: "ts",
     name: "TypeScript",
     level: "Beginner",
-    image: "/images/logos/logo-typescript.svg",
+    image: "images/logos/logo-typescript.svg",
   },
 ];
 
@@ -42,43 +42,43 @@ export const frameworks = [
     id: "react",
     name: "React",
     level: "Advance",
-    image: "/images/logos/logo-react.svg",
+    image: "images/logos/logo-react.svg",
   },
   {
     id: "flask",
     name: "Flask",
     level: "Advance",
-    image: "/images/logos/logo-flask.svg",
+    image: "images/logos/logo-flask.svg",
   },
   {
     id: "vue",
     name: "Vue",
     level: "Beginner",
-    image: "/images/logos/logo-vue.svg",
+    image: "images/logos/logo-vue.svg",
   },
   {
     id: "flutter",
     name: "Flutter",
     level: "Beginner",
-    image: "/images/logos/logo-flutter.svg",
+    image: "images/logos/logo-flutter.svg",
   },
   {
     id: "keras",
     name: "Keras",
     level: "Intermediate",
-    image: "/images/logos/logo-keras.svg",
+    image: "images/logos/logo-keras.svg",
   },
   {
     id: "express",
     name: "Express",
     level: "Advance",
-    image: "/images/logos/logo-express.svg",
+    image: "images/logos/logo-express.svg",
   },
   {
     id: "tensorflow",
     name: "Tensorflow",
     level: "Beginner",
-    image: "/images/logos/logo-tensorflow.svg",
+    image: "images/logos/logo-tensorflow.svg",
   },
 ];
 
@@ -86,32 +86,32 @@ export const other = [
   {
     name: "Linux",
     level: "Advance",
-    image: "/images/logos/logo-linux.svg",
+    image: "images/logos/logo-linux.svg",
   },
   {
     name: "Git",
     level: "Advance",
-    image: "/images/logos/logo-git.svg",
+    image: "images/logos/logo-git.svg",
   },
   {
     name: "Unreal",
     level: "Beginner",
-    image: "/images/logos/logo-unreal.svg",
+    image: "images/logos/logo-unreal.svg",
   },
   {
     name: "Unity",
     level: "Beginner",
-    image: "/images/logos/logo-unity.svg",
+    image: "images/logos/logo-unity.svg",
   },
   {
     name: "Blender",
     level: "Intermediate",
-    image: "/images/logos/logo-blender.svg",
+    image: "images/logos/logo-blender.svg",
   },
   {
     name: "Arduino",
     level: "Advance",
-    image: "/images/logos/logo-arduino.svg",
+    image: "images/logos/logo-arduino.svg",
   },
 ];
 
