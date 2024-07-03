05f08b9b0ce61cc1fcbb1ffb20b406c3803d02be
userabhibhullar <iabhibhullaryt@gmail.com>
2024-07-03 12:06:00 +0530

    hamburger menu toggles on click and removed unwanted space from hero section

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
 import { renderProjects } from "./render/renderProjects.js";
 import { renderTechCards } from "./render/renderTechnologies.js";
 
+var hamburger = document.querySelector(".check");
+var navMenu = document.querySelector(".header-links");
+
+hamburger.addEventListener("change", () => {
+  navMenu.classList.toggle("opened");
+});
+
+navMenu.addEventListener("click", () => {
+  navMenu.classList.remove("opened");
+  hamburger.checked = false;
+});
+
 renderTechCards();
 renderProjects();
